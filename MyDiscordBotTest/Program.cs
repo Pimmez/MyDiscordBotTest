1fc0f015db9958b14afb0a514091f41ea1634bfe
Pim Horeman <pimhoreman@gmail.com>
2017-04-11 17:50:56 +0200

    My Discord Bot
    Just A Discord Bot In The Making For Fun!

---
+++ MyDiscordBotTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyDiscordBotTest;
+using Discord;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThisDiscord bot = new ThisDiscord();
+        }
+    }
+}
